99440d18fd3f619bae289b01f9968439df39998b
Michael Verdicchio <verdicchio@gmail.com>
2016-10-21 17:24:45 -0400

    Add files via upload

---
+++ ArrayIntro.java
@@ -0,0 +1,36 @@
+// YOUR NAME:
+
+// This class is to help you practice looping through arrays.
+// Do not change any code provided. Only add to it.
+public class ArrayIntro {
+
+  public static void main(String[] args) {
+    // create an array to hold 10 000 integers:
+    int[] numbers = new int[10000];
+
+    // fill the array with random integers between 0 and 1000
+    // WRITE YOUR CODE HERE:
+
+
+
+    // add three to every element in the array
+    // WRITE YOUR CODE HERE:
+
+
+
+
+    // here are some variables; don't change them.
+    int countEven = 0;
+    int countOdd = 0;
+
+    // count the number of even/odd elements in the array.
+    // use my variables above.
+    // WRITE YOUR CODE HERE
+
+
+    
+
+    // print some stats:
+    System.out.printf("The array had %d even and %d odd numbers.\n", countEven, countOdd);
+  }
+}
